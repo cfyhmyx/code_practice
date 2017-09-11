c1e38e1863aaabca3622f02e92eb0dd9960fbbea
cfyhmyx <cfyhmyx@gmail.com>
2017-09-10 18:57:46 -0700

    add description

--- Leetcode647.java
+++ Leetcode647.java
@@ -1,3 +1,6 @@
+//Given a string, your task is to count how many palindromic substrings in this string.
+//The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
 import java.util.ArrayList;
 import java.util.List;
 
