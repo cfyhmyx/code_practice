33dc2ddd2e7b9beaf3c1b888c9f7029218f6b026
cfyhmyx <cfyhmyx@gmail.com>
2017-09-11 23:56:44 -0700

    leetcode301

---
+++ Leetcode301.java
@@ -0,0 +1,39 @@
+//Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode301 {
+    public static void main(String args[]) {
+        List<String> result = removeInvalidParentheses("((()");
+        for(int i=0; i<result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+
+    public static List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList<>();
+        remove(s, ans, 0, 0, new char[]{'(', ')'});
+        return ans;
+    }
+
+    public static void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+        for (int stack = 0, i = last_i; i < s.length(); ++i) {
+            if (s.charAt(i) == par[0]) stack++;
+            if (s.charAt(i) == par[1]) stack--;
+            if (stack >= 0) continue;
+            for (int j = last_j; j <= i; ++j) {
+                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+            }
+            return;
+        }
+        String reversed = new StringBuilder(s).reverse().toString();
+        if (par[0] == '(') {// finished left to right
+            remove(reversed, ans, 0, 0, new char[]{')', '('});
+        } else { // finished right to left
+            ans.add(reversed);
+        }
+    }
+
+}
