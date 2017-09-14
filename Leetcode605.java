f652c73e954f2a6aa1086fdeef5b1427e8d39d50
cfyhmyx <cfyhmyx@gmail.com>
2017-09-13 23:46:18 -0700

    leetcode605

---
+++ Leetcode605.java
@@ -0,0 +1,31 @@
+//Suppose you have a long flowerbed in which some of the plots are planted and some are not.
+// However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+//Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n,
+// return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+import java.util.*;
+
+public class Leetcode605 {
+    public static void main(String args[]) {
+        int[] flowerbed = {1,0,0,0,0,1};
+        boolean result = canPlaceFlowers(flowerbed, 2);
+        System.out.println(result);
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for(int i = 0; i < flowerbed.length && count < n; i++) {
+            if(flowerbed[i] == 0) {
+                //get next and prev flower bed slot values. If i lies at the ends the next and prev are considered as 0.
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                if(next == 0 && prev == 0) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+
+        return count >= n;
+    }
+}
