8f258d3463a9c399cbb549a34d3c1ae419a235ff
cfyhmyx <cfyhmyx@gmail.com>
2017-09-22 22:35:19 -0700

    leetcode525

---
+++ cp525_contiguous_array.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+public class Leetcode525 {
+    public static void main(String args[]) {
+
+    }
+
+    public static int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int zero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) zero++;
+            else zero--;
+            if (zero == 0) max = i + 1;
+            if (!map.containsKey(zero)) map.put(zero, i);
+            //find the same key means the array between last position and current position has equal number of 0 and 1
+            else max = Math.max(max, i - map.get(zero));
+        }
+        return max;
+    }
+}
