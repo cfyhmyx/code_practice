e451d1a4dd2e6a91deea6c46fa6014adf8903cc6
cfyhmyx <cfyhmyx@gmail.com>
2017-09-24 21:13:00 -0700

    leetcode141

---
+++ cp141_linked_list_cycle.java
@@ -0,0 +1,37 @@
+//Given a linked list, determine if it has a cycle in it.
+public class Leetcode141 {
+    public static void main(String args[]) {
+        ListNode n1 = new ListNode(0);
+        ListNode n2 = new ListNode(1);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(3);
+        ListNode n5 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n3;
+
+        boolean result = hasCycle(n1);
+        System.out.println(result);
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
