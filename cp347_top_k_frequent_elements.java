6596b64e82f1e78fee9784673304d546a5f757ad
cfyhmyx <cfyhmyx@gmail.com>
2017-09-30 22:43:59 -0700

    leetcode347

---
+++ cp347_top_k_frequent_elements.java
@@ -0,0 +1,37 @@
+//Given a non-empty array of integers, return the k most frequent elements.
+//You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+//Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+import java.util.*;
+
+public class Leetcode347 {
+    public static void main(String args[]) {
+
+    }
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+        return res;
+    }
+
+}
