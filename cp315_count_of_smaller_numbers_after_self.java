fccfaa2b73bf090c42edb45958488dd24ae9abd4
cfyhmyx <cfyhmyx@gmail.com>
2017-10-25 20:40:14 -0700

    leetcode315

---
+++ cp315_count_of_smaller_numbers_after_self.java
@@ -0,0 +1,109 @@
+//You are given an integer array nums and you have to return a new counts array.
+//The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+//explaination: https://discuss.leetcode.com/topic/31405/9ms-short-java-bst-solution-get-answer-when-building-bst
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode315 {
+    public static void main(String args[]) {
+        int[] nums = {5,2,6,1};
+        List<Integer> result = countSmaller(nums);
+        System.out.println(result);
+    }
+
+    static class Node {
+        Node left, right;
+        int val, sum, dup = 1;
+        public Node(int v, int s) {
+            val = v;
+            sum = s;
+        }
+    }
+
+    public static List<Integer> countSmaller(int[] nums) {
+        Integer[] ans = new Integer[nums.length];
+        Node root = null;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            root = insert(nums[i], root, ans, i, 0);
+        }
+        return Arrays.asList(ans);
+    }
+
+    private static Node insert(int num, Node node, Integer[] ans, int i, int preSum) {
+        if (node == null) {
+            node = new Node(num, 0);
+            ans[i] = preSum;
+        } else if (node.val == num) {
+            node.dup++;
+            ans[i] = preSum + node.sum;
+        } else if (node.val > num) {
+            node.sum++;
+            node.left = insert(num, node.left, ans, i, preSum);
+        } else {
+            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+        }
+        return node;
+    }
+
+   /* static int[] count;
+    public static List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+
+        count = new int[nums.length];
+        int[] indexes = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            indexes[i] = i;
+        }
+        mergesort(nums, indexes, 0, nums.length - 1);
+        for(int i = 0; i < count.length; i++){
+            res.add(count[i]);
+        }
+        return res;
+    }
+    private static void mergesort(int[] nums, int[] indexes, int start, int end){
+        if(end <= start){
+            return;
+        }
+        int mid = (start + end) / 2;
+        mergesort(nums, indexes, start, mid);
+        mergesort(nums, indexes, mid + 1, end);
+
+        merge(nums, indexes, start, end);
+    }
+    private static void merge(int[] nums, int[] indexes, int start, int end){
+        int mid = (start + end) / 2;
+        int left_index = start;
+        int right_index = mid+1;
+        int rightcount = 0;
+        int[] new_indexes = new int[end - start + 1];
+
+        int sort_index = 0;
+        while(left_index <= mid && right_index <= end){
+            if(nums[indexes[right_index]] < nums[indexes[left_index]]){
+                new_indexes[sort_index] = indexes[right_index];
+                rightcount++;
+                right_index++;
+            }else{
+                new_indexes[sort_index] = indexes[left_index];
+                count[indexes[left_index]] += rightcount;
+                left_index++;
+            }
+            sort_index++;
+        }
+        while(left_index <= mid){
+            new_indexes[sort_index] = indexes[left_index];
+            count[indexes[left_index]] += rightcount;
+            left_index++;
+            sort_index++;
+        }
+        while(right_index <= end){
+            new_indexes[sort_index++] = indexes[right_index++];
+        }
+        for(int i = start; i <= end; i++){
+            indexes[i] = new_indexes[i - start];
+        }
+    }*/
+
+}
