cdd75539a4e98bddf62d9f7ba95288725f894463
cfyhmyx <cfyhmyx@gmail.com>
2017-11-05 15:12:01 -0800

    leetcode292

---
+++ cp292_nim_game.java
@@ -0,0 +1,12 @@
+//description: https://leetcode.com/problems/nim-game/description/
+
+public class Leetcode292 {
+    public static void main(String args[]) {
+
+    }
+
+    public boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+
+}
