0630bef94c4dfe590b11eefe307c1876440d119e
cfyhmyx <cfyhmyx@gmail.com>
2017-11-29 23:17:09 -0800

    leetcode236

---
+++ cp236_lowest_common_ancestor_of_a_binary_tree.java
@@ -0,0 +1,33 @@
+//Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+//According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the
+//lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+public class Leetcode236 {
+    public static void main(String args[]) {
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left!=null && right!=null) {
+            return root;
+        } else if(left != null) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+
+     public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
