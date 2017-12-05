4a74f0f334380f9e7be0637fba3eecbb98052c3a
cfyhmyx <cfyhmyx@gmail.com>
2017-12-05 00:02:43 -0800

    leetcode341

---
+++ cp341_flatten_nested_list_iterator.java
@@ -0,0 +1,55 @@
+//Given a nested list of integers, implement an iterator to flatten it.
+//Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+import java.util.*;
+
+public class Leetcode341 {
+    public static void main(String args[]) {
+
+    }
+
+    public class NestedIterator implements Iterator<Integer> {
+
+        Stack<NestedInteger> stack;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            stack = new Stack<>();
+            for(int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return stack.pop().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while(!stack.isEmpty()) {
+                NestedInteger curr = stack.peek();
+                if(curr.isInteger()) {
+                    return true;
+                }
+                stack.pop();
+                for(int i = curr.getList().size() - 1; i >= 0; i--) {
+                    stack.push(curr.getList().get(i));
+                }
+            }
+            return false;
+        }
+    }
+
+    public interface NestedInteger {
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        //@return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+}
