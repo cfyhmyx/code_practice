643304af1454dc8ab7af49432564d216d753f18e
cfyhmyx <cfyhmyx@gmail.com>
2017-12-07 21:19:24 -0800

    leetcode647

---
+++ cp647_palindromic_substrings.java
@@ -0,0 +1,31 @@
+//Given a string, your task is to count how many palindromic substrings in this string.
+//The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+public class Leetcode647 {
+    public static void main(String args[]) {
+        String s = "aaa";
+        int total = countSubstrings(s);
+        System.out.println(total);
+    }
+
+    public static int countSubstrings(String s) {
+        int m = s.length();
+        int[][] cash = new int[m][m];
+        int total = 0;
+
+        for(int j=0; j<m; j++) {
+            for(int i=0; i<=j; i++) {
+                 if(i==j) {
+                     total += 1;
+                     cash[i][j] = 1;
+                 } else if(s.charAt(i) == s.charAt(j)){
+                     if(i+1 == j || cash[i+1][j-1] == 1) {
+                         cash[i][j] = 1;
+                         total += 1;
+                     }
+                 }
+            }
+        }
+        return total;
+    }
+}
