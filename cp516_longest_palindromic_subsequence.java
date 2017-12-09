c4684dc7b6f7358feed25b389c4820ff3369d60f
cfyhmyx <cfyhmyx@gmail.com>
2017-12-08 18:18:26 -0800

    improve leetcode516

--- cp516_longest_palindromic_subsequence.java
+++ cp516_longest_palindromic_subsequence.java
@@ -7,19 +7,22 @@ public class Leetcode516 {
     }
 
     public static int longestPalindromeSubseq(String s) {
-        int[][] dp = new int[s.length()][s.length()];
-
-        for (int i = s.length() - 1; i >= 0; i--) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++) {
             dp[i][i] = 1;
-            for (int j = i+1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j)) {
+        }
+        for(int length = 1; length < n; length++) {
+            for(int i=0; i<n-length; i++) {
+                int j = i+length;
+                if(s.charAt(i) == s.charAt(j)) {
                     dp[i][j] = dp[i+1][j-1] + 2;
                 } else {
                     dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
-        return dp[0][s.length()-1];
+        return dp[0][n-1];
     }
 
 }
