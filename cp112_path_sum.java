ab834cc86f8fa7792a1ac0f9b91a8419cb0c9f1e
cfyhmyx <cfyhmyx@gmail.com>
2017-12-08 19:26:33 -0800

    leetcode112

---
+++ cp112_path_sum.java
@@ -0,0 +1,50 @@
+//Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that
+//adding up all the values along the path equals the given sum.
+
+public class Leetcode112 {
+    public static void main(String args[]) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(5);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n5 = new TreeNode(4);
+        TreeNode n6 = new TreeNode(4);
+        TreeNode n7 = new TreeNode(32);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+
+        boolean result = hasPathSum(n1, 38);
+        System.out.println(result);
+    }
+
+    /*public static boolean hasPathSum(TreeNode root, int sum) {
+        return helper(root, sum);
+    }
+
+    public static boolean helper(TreeNode current, int sum) {
+        if(current == null) return false;
+        if(current.left == null && current.right == null) {
+            return sum-current.val == 0;
+        }
+        return helper(current.left, sum-current.val) || helper(current.right, sum-current.val);
+    }*/
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null) {
+            return sum-root.val == 0;
+        }
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+    }
+
+    public  static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
