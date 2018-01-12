cbf875c5ae7daf04405aa99c0580261ff77fd3f9
cfyhmyx <cfyhmyx@gmail.com>
2018-01-11 22:22:47 -0800

    leetcode311

---
+++ cp311_sparse_matrix_multiplication.java
@@ -0,0 +1,24 @@
+//Given two sparse matrices A and B, return the result of AB.
+//You may assume that A's column number is equal to B's row number.
+
+public class Leetcode311 {
+    public static void main(String args[]) {
+    }
+
+    public static int[][] multiply(int[][] A, int[][] B) {
+        int m = A.length, n = A[0].length, nB = B[0].length;
+        int[][] C = new int[m][nB];
+
+        for(int i = 0; i < m; i++) {
+            for(int k = 0; k < n; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < nB; j++) {
+                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return C;
+    }
+
+}
