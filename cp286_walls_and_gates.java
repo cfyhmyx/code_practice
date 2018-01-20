360eb472596be8f7a344e828a7b3e706d43747fe
cfyhmyx <cfyhmyx@gmail.com>
2018-01-19 22:34:52 -0800

    start from certrain elements, BFS>DFS

--- cp286_walls_and_gates.java
+++ cp286_walls_and_gates.java
@@ -1,11 +1,14 @@
 //https://leetcode.com/problems/walls-and-gates/description/
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Leetcode286 {
     public static void main(String args[]) {
         int[][] grid = {{Integer.MAX_VALUE,-1,0,Integer.MAX_VALUE},
-                        {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,-1},
-                        {Integer.MAX_VALUE,-1,Integer.MAX_VALUE,-1},
-                        {0,-1,Integer.MAX_VALUE,Integer.MAX_VALUE}};
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,-1},
+                {Integer.MAX_VALUE,-1,Integer.MAX_VALUE,-1},
+                {0,-1,Integer.MAX_VALUE,Integer.MAX_VALUE}};
         wallsAndGates(grid);
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
@@ -16,7 +19,35 @@ public class Leetcode286 {
         }
     }
 
+    //bfs O(number of elements)
     public static void wallsAndGates(int[][] rooms) {
+        int m = rooms.length;
+        int n = m == 0 ? 0 : rooms[0].length;
+        int[][] dirs = {{-1,0}, {0,1}, {0,-1}, {1,0}};
+        Queue<int[]> queue = new LinkedList<>();
+        // add all gates to the queue
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(new int[] {i,j});
+                }
+            }
+        }
+        // update distance from gates
+        while (!queue.isEmpty()) {
+            int[] curPos = queue.poll();
+            for (int[] dir: dirs) {
+                int X = curPos[0] + dir[0];
+                int Y = curPos[1] + dir[1];
+                if (X<0 || Y <0 || X >= m || Y >= n || rooms[X][Y] <= rooms[curPos[0]][curPos[1]]+1) continue;
+                rooms[X][Y] = rooms[curPos[0]][curPos[1]]+1;
+                queue.offer(new int[] {X, Y});
+            }
+        }
+    }
+
+    //dfs slower >O(number of elements)
+    /*public static void wallsAndGates(int[][] rooms) {
         int m = rooms.length;
         if(m==0) return;
         int n = rooms[0].length;
@@ -36,5 +67,5 @@ public class Leetcode286 {
         findDistance(row+1, col, m, n, rooms, dis+1);
         findDistance(row, col-1, m, n, rooms, dis+1);
         findDistance(row, col+1, m, n, rooms, dis+1);
-    }
+    }*/
 }
