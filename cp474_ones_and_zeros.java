e9ae8d35f9b3ef09733ea4d3a33ab112ae5b60b3
cfyhmyx <cfyhmyx@gmail.com>
2018-01-25 12:37:40 -0800

    0/1 knapsack dp

--- cp474_ones_and_zeros.java
+++ cp474_ones_and_zeros.java
@@ -1,4 +1,6 @@
-//description: https://leetcode.com/problems/heaters/description/
+//In the computer world, use restricted resource you have to generate maximum benefit is what we always want to pursue.
+//For now, suppose you are a dominator of m 0s and n 1s respectively. On the other hand, there is an array with strings consisting of only 0s and 1s.
+//Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s. Each 0 and 1 can be used at most once.
 
 public class Leetcode474 {
     public static void main(String args[]) {
