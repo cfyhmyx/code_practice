fac9f427d1345023167aa2bbc19ba7ac9936327d
cfyhmyx <cfyhmyx@gmail.com>
2018-01-25 21:21:57 -0800

    string

---
+++ cp418_sentence_screen_fitting.java
@@ -0,0 +1,25 @@
+//Given a rows x cols screen and a sentence represented by a list of non-empty words, find how many times the given sentence can be fitted on the screen.
+
+public class Leetcode418 {
+    public static void main(String args[]) {
+
+    }
+
+    public static int wordsTyping(String[] sentence, int rows, int cols) {
+        String s = String.join(" ", sentence) + " ";
+        int start = 0, l = s.length();
+        for (int i = 0; i < rows; i++) {
+            start += cols;
+            if (s.charAt(start % l) == ' ') {
+                start++;
+            } else {
+                while (start > 0 && s.charAt((start-1) % l) != ' ') {
+                    start--;
+                }
+            }
+        }
+
+        return start / s.length();
+    }
+
+}
