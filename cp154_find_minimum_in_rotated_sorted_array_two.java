45662f1ed95cd34aaea92ca3eb847de3ec514b49
cfyhmyx <cfyhmyx@gmail.com>
2018-02-04 20:01:22 -0800

    binary search

--- cp154_find_minimum_in_rotated_sorted_array_two.java
+++ cp154_find_minimum_in_rotated_sorted_array_two.java
@@ -5,7 +5,7 @@
 
 public class Leetcode154 {
     public static void main(String args[]) {
-        int[] nums = {10,1,10,10,10};
+        int[] nums = {3,3,1};
         int result = findMin(nums);
         System.out.println(result);
     }
@@ -18,33 +18,16 @@ public class Leetcode154 {
                 return nums[low];
             }
             int mid = low + (high-low)/2;
-            if(nums[mid] > nums[low]) {
+            if(nums[mid] == nums[low] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            } else if(nums[mid] >= nums[low]) {
                 low = mid+1;
             } else if(nums[mid] < nums[low]){
                 high = mid;
-            } else {
-                low++;
             }
         }
         return nums[low];
     }
-
-    //better way
-    /*  int start=0,end=num.size()-1;
-
-        while (start<end) {
-        if (num[start]<num[end])
-            return num[start];
-
-        int mid = (start+end)/2;
-
-        if (num[mid]>=num[start]) {
-            start = mid+1;
-        } else {
-            end = mid;
-        }
-    }
-
-        return num[start];
-        */
+    
 }
