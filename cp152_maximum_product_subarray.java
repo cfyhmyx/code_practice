8a7503f4f46f137a8c40de8de5c3cb68eb2c3f5c
cfyhmyx <cfyhmyx@gmail.com>
2018-02-04 19:35:11 -0800

    dp

--- cp152_maximum_product_subarray.java
+++ cp152_maximum_product_subarray.java
@@ -1,3 +1,6 @@
+//Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+//For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has the largest product = 6.
+
 public class Leetcode152 {
     public static void main(String args[]) {
         int[] nums = {2,3,-2,-1,0,13};
