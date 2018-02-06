7659d6f24c5a429603dce2c8d70754a9086d2ecc
cfyhmyx <cfyhmyx@gmail.com>
2018-02-05 22:24:27 -0800

    dp

--- cp198_house_robber.java
+++ cp198_house_robber.java
@@ -7,7 +7,20 @@ public class Leetcode198 {
         System.out.println(result);
     }
 
+    //O(1) space
     public static int rob(int[] nums) {
+        int prevNo = 0;
+        int prevYes = 0;
+        for(int num : nums) {
+            int temp = prevNo;
+            prevNo = Math.max(prevNo, prevYes);
+            prevYes = temp + num;
+        }
+        return Math.max(prevNo, prevYes);
+    }
+
+    //o(n) space
+    /*public static int rob(int[] nums) {
         int m = nums.length;
         if(m<1) return 0;
         int[] dp = new int[m+1];
@@ -17,6 +30,6 @@ public class Leetcode198 {
             dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i-1]);
         }
         return dp[m];
-    }
+    }*/
 
 }
