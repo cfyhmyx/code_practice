b76a650a96df08892b70e99fbdf91c50686bbb66
cfyhmyx <cfyhmyx@gmail.com>
2018-02-10 10:27:16 -0800

    slow fast pointer, reverser linked list

--- cp234_palindrome_linked_list.java
+++ cp234_palindrome_linked_list.java
@@ -1,3 +1,6 @@
+//Given a singly linked list, determine if it is a palindrome.
+//Follow up: Could you do it in O(n) time and O(1) space?
+
 public class Leetcode234 {
     public static void main(String args[]) {
 
