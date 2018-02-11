483b25d53c16b43bde40e3fbbda93e30789bbdd6
cfyhmyx <cfyhmyx@gmail.com>
2018-02-11 13:19:20 -0800

    inorder traversal or find predecessor and successor

---
+++ cp272_closest_binary_search_tree_value_two.java
@@ -0,0 +1,119 @@
+//Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+//Note:
+//Given target value is a floating point.
+//You are guaranteed to have only one unique value in the BST that is closest to the target.
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Leetcode272 {
+    public static void main(String args[]) {
+
+    }
+
+    //O(n)
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        LinkedList<Integer> res = new LinkedList<>();
+        helper(res, root, target, k);
+        return res;
+    }
+
+    private void helper(LinkedList<Integer> res, TreeNode root, double target, int k) {
+        if(root == null) return;
+        helper(res, root.left, target, k);
+        if(res.size() == k) {
+            if(Math.abs(target-root.val)<Math.abs(target-res.peekFirst())) {
+                res.removeFirst();
+            } else {
+                return;
+            }
+        }
+        res.add(root.val);
+        helper(res, root.right, target, k);
+    }
+
+    //O(logn)
+    /*public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> ret = new LinkedList<>();
+        Stack<TreeNode> succ = new Stack<>();
+        Stack<TreeNode> pred = new Stack<>();
+        initializePredecessorStack(root, target, pred);
+        initializeSuccessorStack(root, target, succ);
+        if(!succ.isEmpty() && !pred.isEmpty() && succ.peek().val == pred.peek().val) {
+            getNextPredecessor(pred);
+        }
+        while(k-- > 0) {
+            if(succ.isEmpty()) {
+                ret.add(getNextPredecessor(pred));
+            } else if(pred.isEmpty()) {
+                ret.add(getNextSuccessor(succ));
+            } else {
+                double succ_diff = Math.abs((double)succ.peek().val - target);
+                double pred_diff = Math.abs((double)pred.peek().val - target);
+                if(succ_diff < pred_diff) {
+                    ret.add(getNextSuccessor(succ));
+                } else {
+                    ret.add(getNextPredecessor(pred));
+                }
+            }
+        }
+        return ret;
+    }
+
+    private void initializeSuccessorStack(TreeNode root, double target, Stack<TreeNode> succ) {
+        while(root != null) {
+            if(root.val == target) {
+                succ.push(root);
+                break;
+            } else if(root.val > target) {
+                succ.push(root);
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+    }
+
+    private void initializePredecessorStack(TreeNode root, double target, Stack<TreeNode> pred) {
+        while(root != null){
+            if(root.val == target){
+                pred.push(root);
+                break;
+            } else if(root.val < target){
+                pred.push(root);
+                root = root.right;
+            } else{
+                root = root.left;
+            }
+        }
+    }
+
+    private int getNextSuccessor(Stack<TreeNode> succ) {
+        TreeNode curr = succ.pop();
+        int ret = curr.val;
+        curr = curr.right;
+        while(curr != null) {
+            succ.push(curr);
+            curr = curr.left;
+        }
+        return ret;
+    }
+
+    private int getNextPredecessor(Stack<TreeNode> pred) {
+        TreeNode curr = pred.pop();
+        int ret = curr.val;
+        curr = curr.left;
+        while(curr != null) {
+            pred.push(curr);
+            curr = curr.right;
+        }
+        return ret;
+    }*/
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
