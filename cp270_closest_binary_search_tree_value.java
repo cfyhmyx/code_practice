8f358e5f54c424e537e8761d7d54f911a1f04b2f
cfyhmyx <cfyhmyx@gmail.com>
2018-02-11 12:17:29 -0800

    BST

---
+++ cp270_closest_binary_search_tree_value.java
@@ -0,0 +1,29 @@
+//Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+//Note:
+//Given target value is a floating point.
+//You are guaranteed to have only one unique value in the BST that is closest to the target.
+
+public class Leetcode270 {
+    public static void main(String args[]) {
+
+    }
+
+    public int closestValue(TreeNode root, double target) {
+        int ret = root.val;
+        while(root != null){
+            if(Math.abs(target - root.val) < Math.abs(target - ret)){
+                ret = root.val;
+            }
+            root = root.val > target? root.left: root.right;
+        }
+        return ret;
+    }
+
+
+    public  static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
