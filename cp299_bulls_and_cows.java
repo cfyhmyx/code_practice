bcc9ad5e2482816afdff446f9bd5d4977182da68
cfyhmyx <cfyhmyx@gmail.com>
2018-02-12 22:04:12 -0800

    hashmap

---
+++ cp299_bulls_and_cows.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/bulls-and-cows/description/
+
+public class Leetcode299 {
+    public static void main(String args[]) {
+
+    }
+
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i<secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else {
+                if (numbers[secret.charAt(i)-'0']++ < 0) cows++;
+                if (numbers[guess.charAt(i)-'0']-- > 0) cows++;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
