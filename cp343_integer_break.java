6ce7dec23bc5c186e9a20c22d56f0a05115ddf4e
cfyhmyx <cfyhmyx@gmail.com>
2018-02-18 16:52:33 -0800

    analyze

---
+++ cp343_integer_break.java
@@ -0,0 +1,18 @@
+//Given an integer, write a function to determine if it is a power of four.
+
+public class Leetcode343 {
+    public static void main(String args[]) {
+
+    }
+
+    public int integerBreak(int n) {
+        if(n == 2 || n == 3) return n-1;
+        int res = 1;
+        while(n>4) {
+            res *= 3;
+            n -= 3;
+        }
+        return res*n;
+    }
+
+}
