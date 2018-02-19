776da44425297db550fe8717ac1223ed42af2ff8
cfyhmyx <cfyhmyx@gmail.com>
2018-02-18 16:53:24 -0800

    two pointer

---
+++ cp345_reverse_vowels_of_a_string.java
@@ -0,0 +1,31 @@
+//Write a function that takes a string as input and reverse only the vowels of a string.
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Leetcode345 {
+    public static void main(String args[]) {
+
+    }
+
+    public String reverseVowels(String s) {
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            if (!set.contains(arr[left])) {
+                left++;
+            } else if (!set.contains(arr[right])) {
+                right--;
+            } else {
+                char tmp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}
