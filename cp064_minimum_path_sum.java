39a0dd022c5d4d9e654fa5400f186c354c52d21b
cfyhmyx <cfyhmyx@gmail.com>
2018-03-06 22:09:57 -0800

    dp

---
+++ cp064_minimum_path_sum.java
@@ -0,0 +1,29 @@
+//Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right
+//which minimizes the sum of all numbers along its path.
+
+public class Leetcode064 {
+    public static void main(String args[]) {
+        int[][] nums = {{0,1}, {1,0}};
+        int result = minPathSum(nums);
+        System.out.println(result);
+    }
+
+    public static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<m; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j=1; j<n; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        for (int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
