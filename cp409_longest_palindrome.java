558574bc9b2d346c5fe9f6032ee2619364a65d0e
cfyhmyx <cfyhmyx@gmail.com>
2018-03-17 14:28:44 -0700

    hashset

---
+++ cp409_longest_palindrome.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/longest-palindrome/description/
+
+import java.util.HashSet;
+
+public class Leetcode409 {
+    public static void main(String args[]) {
+    }
+
+    public int longestPalindrome(String s) {
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
+            }else{
+                hs.add(s.charAt(i));
+            }
+        }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
+    }
+
+}
