0089fcaaac5c12f7ca60eaa900bea1ee653f1468
cfyhmyx <cfyhmyx@gmail.com>
2018-04-01 01:55:29 -0700

    dp

---
+++ cp518_coin_change_two.java
@@ -0,0 +1,34 @@
+//You are given coins of different denominations and a total amount of money.
+//Write a function to compute the number of combinations that make up that amount.
+//You may assume that you have infinite number of each kind of coin.
+
+public class Leetcode518 {
+    public static void main(String args[]) {
+
+    }
+
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= coins.length; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i-1][j] + (j >= coins[i-1] ? dp[i][j-coins[i-1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
+     }
+
+    /*public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }*/
+
+}
