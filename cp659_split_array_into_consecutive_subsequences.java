a8da973e4ef418f4b1501daa29222bcba59d955c
cfyhmyx <cfyhmyx@gmail.com>
2018-04-03 18:49:14 -0700

    hashmap

--- cp659_split_array_into_consecutive_subsequences.java
+++ cp659_split_array_into_consecutive_subsequences.java
@@ -1,32 +1,33 @@
 //You are given an integer array sorted in ascending order (may contain duplicates), you need to split them into several subsequences,
 //where each subsequences consist of at least 3 consecutive integers. Return whether you can make such a split.
-//explaination: https://discuss.leetcode.com/topic/99495/java-o-n-time-o-1-space-solution
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetcode659 {
     public static void main(String args[]) {
-        int[] nums = {1,2,3,4};
+        int[] nums = {1,2,3,3,4,4,5,5};
         boolean result = isPossible(nums);
         System.out.println(result);
     }
 
     public static boolean isPossible(int[] nums) {
-        int pre = Integer.MIN_VALUE, p1 = 0, p2 = 0, p3 = 0;
-        int cur = 0, cnt = 0, c1 = 0, c2 = 0, c3 = 0;
-
-        for (int i = 0; i < nums.length; pre = cur, p1 = c1, p2 = c2, p3 = c3) {
-            for (cur = nums[i], cnt = 0; i < nums.length && cur == nums[i]; cnt++, i++);
-
-            if (cur != pre + 1) {
-                if (p1 != 0 || p2 != 0) return false;
-                c1 = cnt; c2 = 0; c3 = 0;
-
-            } else {
-                if (cnt < p1 + p2) return false;
-                c1 = Math.max(0, cnt - (p1 + p2 + p3));
-                c2 = p1;
-                c3 = p2 + Math.min(p3, cnt - (p1 + p2));
+        Map<Integer, Integer> freq = new HashMap<>(), appendfreq = new HashMap<>();
+        for (int i : nums) freq.put(i, freq.getOrDefault(i,0) + 1);
+        for (int i : nums) {
+            if (freq.get(i) == 0) continue;
+            else if (appendfreq.getOrDefault(i,0) > 0) {
+                appendfreq.put(i, appendfreq.get(i) - 1);
+                appendfreq.put(i+1, appendfreq.getOrDefault(i+1,0) + 1);
             }
+            else if (freq.getOrDefault(i+1,0) > 0 && freq.getOrDefault(i+2,0) > 0) {
+                freq.put(i+1, freq.get(i+1) - 1);
+                freq.put(i+2, freq.get(i+2) - 1);
+                appendfreq.put(i+3, appendfreq.getOrDefault(i+3,0) + 1);
+            }
+            else return false;
+            freq.put(i, freq.get(i) - 1);
         }
-
-        return p1 == 0 && p2 == 0;
+        return true;
     }
 }
