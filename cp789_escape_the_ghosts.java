35ec8ad3674decc8119bfeb073ff20b12f7ba0ef
cfyhmyx <cfyhmyx@gmail.com>
2018-04-04 20:16:32 -0700

    analyze

---
+++ cp789_escape_the_ghosts.java
@@ -0,0 +1,17 @@
+//Given string S and a dictionary of words words, find the number of words[i] that is a subsequence of S.
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Leetcode789 {
+    public static void main(String args[]) {
+
+    }
+
+    public int numTilings(int N) {
+
+    }
+
+}
