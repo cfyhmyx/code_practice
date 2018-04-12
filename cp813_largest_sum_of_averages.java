0cc61f120bf20c274cf14dc4f62f6b02690384dd
cfyhmyx <cfyhmyx@gmail.com>
2018-04-11 21:25:08 -0700

    dp

---
+++ cp813_largest_sum_of_averages.java
@@ -0,0 +1,30 @@
+//We partition a row of numbers A into at most K adjacent (non-empty) groups, then our score is the sum of the average of each group.
+//What is the largest score we can achieve? Note that our partition must use every number in A, and that scores are not necessarily integers.
+
+public class Leetcode813 {
+    public static void main(String args[]) {
+
+    }
+
+    public double largestSumOfAverages(int[] A, int K) {
+        int N = A.length;
+        double[][] memo = new double[N+1][N+1];
+        double cur = 0;
+        for (int i = 0; i < N; ++i) {
+            cur += A[i];
+            memo[i + 1][1] = cur / (i + 1);
+        }
+        return search(N, K, A, memo);
+    }
+
+    public double search(int n, int k, int[] A, double[][] memo) {
+        if (memo[n][k] > 0) return memo[n][k];
+        double cur = 0;
+        for (int i = n - 1; i > 0; --i) {
+            cur += A[i];
+            memo[n][k] = Math.max(memo[n][k], search(i, k - 1, A, memo) + cur / (n - i));
+        }
+        return memo[n][k];
+    }
+
+}
